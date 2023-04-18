d127915024636406720c6d5d0bfa79c141ce71fc
Dmitry Litvinenko <ds-sev@ya.ru>
2023-04-18 17:20:15 +0300

    feat: create models for user and card

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const express = require('express');
 // eslint-disable-next-line import/no-extraneous-dependencies
 const mongoose = require('mongoose');
 
+mongoose.connect('mongodb://localhost:27017/mestodb', {});
+
 const { PORT = 3000 } = process.env;
 const app = express();
-
-mongoose.connect('mongodb://localhost:27017/mestodb', {});
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
