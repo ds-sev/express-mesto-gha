cdaa90ec9e7ee13063bfd8548e0c2dd57944cf69
Dmitry Litvinenko <ds-sev@ya.ru>
2023-04-18 23:25:25 +0300

    feat: create db and testing connection to it

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
-const express = require('express');
-// eslint-disable-next-line import/no-extraneous-dependencies
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/mestodb', {});
+const express = require('express');
+
+const bodyParser = require('body-parser');
+const path = require('path');
 
 const { PORT = 3000 } = process.env;
 const app = express();
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  // useCreateIndex: true,
+  // useFindAndModify: false,
+})
+  .then(() => console.log('connected'))
+  .catch((err) => console.log(`connection error ${err}`));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/', require('./routes/users'));
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
--- controllers/users.js
+++ controllers/users.js
@@ -7,7 +7,7 @@ const User = require('../models/user');
 module.exports.getUser = (req, res) => {
   User.findById(req.params.id)
     .then((user) => res.send({ data: user }))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+    .catch((err) => res.status(500).send({ message: `Произошла ошибка ${err}` }));
 };
 
 module.exports.getUsers = () => {
@@ -18,5 +18,5 @@ module.exports.createUser = (req, res) => {
   const { name, about, avatar } = req.body;
   User.create({ name, about, avatar })
     .then((user) => res.send({ data: user }))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+    .catch((err) => res.status(500).send({ message: `Произошла ошибка ${err}` }));
 };
