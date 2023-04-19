c2cf2ac377680f8c19abecbb25385a84127770d3
Dmitry Litvinenko <ds-sev@ya.ru>
2023-04-19 22:44:44 +0300

    feat: add error handlers for cards controllers

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,41 +1,76 @@
+const mongoose = require('mongoose');
 const Card = require('../models/card');
-
+const { internalServerError, badRequest, notFound } = require('../utils/errors');
+// GET ALL CARDS
 module.exports.getCards = (req, res) => {
   Card.find({})
-  .then((cards) => res.send(cards))
-  .catch((err) => errorsHandler(res, err));
+    .then((cards) => res.send(cards))
+    .catch(() => res.status(internalServerError).send({ message: 'На сервере произошла ошибка.' }));
 };
-
+// CREATE NEW CARD
 module.exports.createCard = (req, res) => {
   console.log(req.user._id);
   const { name, link } = req.body;
   Card.create({ name, link })
     .then((card) => res.send({ data: card }))
-    .catch((err) => res.status(500).send({ message: `Произошла ошибка ${err}` }));
+    .catch((err) => {
+      if (err instanceof mongoose.Error.ValidationError) {
+        res.status(badRequest).send({ message: 'Переданы некорректные данные при создании карточки.' });
+      } else {
+        res.status(internalServerError).send({ message: 'На сервере произошла ошибка.' });
+      }
+    });
 };
-
+// DELETE CARD BY ID
 module.exports.deleteCard = (req, res) => {
   Card.findByIdAndRemove(req.params.id)
+    .orFail(() => {
+      throw new Error('Not found');
+    })
     .then((card) => res.send({ data: card }))
-    .catch((err) => res.status(500).send({ message: `Произошла ошибка ${err}` }));
+    .catch((err) => {
+      if (err.message === 'Not found') {
+        res.status(notFound).send({ message: 'Карточка с указанным id не найдена. ' });
+      } else {
+        res.status(internalServerError).send({ message: 'На сервере произошла ошибка.' });
+      }
+    });
 };
-
+// LIKE CARD BY ID
 module.exports.likeCard = (req, res) => {
   Card.findByIdAndUpdate(
     req.params.id,
     { $addToSet: { likes: req.user._id } },
     { new: true },
   )
+    .orFail(() => {
+      throw new Error('Not found');
+    })
     .then((card) => res.send({ data: card }))
-    .catch((err) => res.status(500).send({ message: `Произошла ошибка ${err}` }));
+    .catch((err) => {
+      if (err.message === 'Not found') {
+        res.status(notFound).send({ message: 'Передан несуществующий id карточки.' });
+      } else {
+        res.status(internalServerError).send({ message: 'На сервере произошла ошибка.' });
+      }
+    });
 };
-
+// DISLIKE CARD BY ID
 module.exports.dislikeCard = (req, res) => {
   Card.findByIdAndUpdate(
     req.params.id,
     { $pull: { likes: req.user._id } },
     { new: true },
   )
+    .orFail(() => {
+      throw new Error('Not found');
+    })
     .then((card) => res.send({ data: card }))
-    .catch((err) => res.status(500).send({ message: `Произошла ошибка ${err}` }));
+    .catch((err) => {
+      if (err.message === 'Not found') {
+        res.status(notFound).send({ message: 'Передан несуществующий id карточки.' });
+      } else {
+        res.status(internalServerError).send({ message: 'На сервере произошла ошибка.' });
+      }
+    });
 };
