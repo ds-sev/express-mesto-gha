7992c62ed0505c1eeea60ab3d6a45ceb026bb7fa
Dmitry Litvinenko <ds-sev@ya.ru>
2023-04-20 10:10:42 +0300

    fix: resolve lint warnings

--- app.js
+++ app.js
@@ -1,18 +1,13 @@
 const mongoose = require('mongoose');
-
 const express = require('express');
-
 const bodyParser = require('body-parser');
-// const path = require('path');
 
 const { PORT = 3000 } = process.env;
 const app = express();
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
-})
-  .then(() => console.log('connected'))
-  .catch((err) => console.log(`connection error ${err}`));
+});
 
 app.use((req, res, next) => {
   req.user = { _id: '643efb70ce70b290ff45b7c1' };
@@ -25,5 +20,5 @@ app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
 
 app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
+  // console.log(`App listening on port ${PORT}`);
 });
