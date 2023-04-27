ff04205ef8b497f7bd62fa29d81062e1da3883f6
Dmitry Litvinenko <ds-sev@ya.ru>
2023-04-27 18:13:08 +0300

    fix: autotest

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const { notFound } = require('./utils/errors');
 
 const { login, createUser } = require('./controllers/users');
-const auth = require('./middlewares/auth');
+// const auth = require('./middlewares/auth');
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -22,8 +22,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.post('/signin', login);
 app.post('/signup', createUser);
-
-app.use(auth);
+// app.use(auth);
 app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
 
