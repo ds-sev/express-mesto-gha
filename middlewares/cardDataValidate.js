296c870ba3fc0312dbd603f28c12d733aef4733a
Dmitry Litvinenko <ds-sev@ya.ru>
2023-04-28 12:43:36 +0300

    fix: add celebrate validation for cards

--- middlewares/cardDataValidate.js
+++ middlewares/cardDataValidate.js
@@ -1,9 +1,9 @@
 const { Joi, celebrate } = require('celebrate')
-const { URL_TEMPLATE } = require('../utils/constants')
+const CUSTOM_PATTERNS = require('../utils/constants')
 
 module.exports.cardDataValidate = celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
-    link: Joi.string().required().pattern(URL_TEMPLATE),
+    link: Joi.string().required().regex(CUSTOM_PATTERNS.URL),
   }),
 })
--- utils/constants.js
+++ utils/constants.js
@@ -1,2 +1,5 @@
-const URL_TEMPLATE = /http(s)?:\/\/(w{3}.)?[a-z0-9.-]+\/[a-z0-9.\-_~:/?#[\]@!$&'()*+,;=]?#?/i
-module.exports = URL_TEMPLATE
+const CUSTOM_PATTERNS = {
+  URL: /http(s)?:\/\/(w{3}.)?[a-z0-9.-]+\/[a-z0-9.\-_~:/?#[\]@!$&'()*+,;=]?#?/i,
+}
+
+module.exports = CUSTOM_PATTERNS
